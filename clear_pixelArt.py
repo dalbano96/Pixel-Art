55d2b6c4ca12f28805b2548a0005ac3d73983292
dalbano96 <dalbano96@gmail.com>
2019-12-10 13:19:26 -1000

    Bug fix

---
+++ clear_pixelArt.py
@@ -0,0 +1,12 @@
+# Daryl Albano
+# 12/10/19
+# stop_pixelArt.py
+# Clears SenseHAT LEDs
+from sense_hat import SenseHat
+
+def clear_pixelArt:
+    sense = SenseHat()
+    sense.set_rotation(r=0)
+    sense.clear()
+
+clear_pixelArt()
--- stop_pixelArt.py
+++
@@ -1,8 +0,0 @@
-# Daryl Albano
-# 12/10/19
-# stop_pixelArt.py
-# Clears SenseHAT LEDs
-from sense_hat import SenseHat
-
-sense.set_rotation(r=0)
-sense.clear()
